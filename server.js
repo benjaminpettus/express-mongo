a5cf3780aef2fde4950af17fddc879cb48239a52
benjaminpettus <benjaminpettus@gmail.com>
2016-02-13 23:34:32 -1000

    working to connect to pixel painter through mongoose

--- server.js
+++ server.js
@@ -1,40 +1,64 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bodyParser = require('body-parser');
+
+var methodOverride = require('method-override');
+
+mongoose.connect('mongodb://localhost/pixeltest');
+
+//schema has one property name:
+var drawingSchema = mongoose.Schema({name: String});
+//model prototype that takes a name and the above schema
+var Drawing = mongoose.model('Drawing', drawingSchema);
 
 var app = express();
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()) ;
 
-mongoose.connect('mongodb://localhost/');
-
-
-app.get('/', function (req, res) {
-  res.send('hi');
+app.get('/drawings/:id', function (req, res) {
+  var drawingId = req.params.id;
+  Drawing.findById(drawingId, function(err, drawing){
+    res.json(drawing);
+  });
 });
 
-// app.get('/drawings', function (req, res) {
+app.put('/drawings/:id', function (req, res) {
+  var drawingId = req.params.id;
+  Drawing.findByIdAndUpdate(drawingId, {
+   $set: {name: req.body.name}
+  },
+  {new: true
+  },
+  function(err, drawing){
+    res.json(drawing);
+  });
+});
 
-// });
+app.get('/drawings', function (req, res) {
+  Drawing.find({}, function(err, drawings){
+    res.json(drawings);
+  });
+});
 
-
-// app.get('/drawings/:id', function (req, res) {
-  
-// });
-
-// app.post('/drawings', function (req, res) {
-  
-// });
-
-// app.put('/drawings', function (req, res) {
-  
-// });
-
-
+app.post('/drawings', function (req, res) {
+  var newDrawing = new Drawing({
+    name: req.body.name
+  });
+  newDrawing.save();
+  res.json(newDrawing);
+});
 
 
 
 
 
-var server = app.listen(3000, function() {
-  console.log('Listening to port', server.address().port);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (){
+  console.log('db connected');
+  app.listen(3000, function() {
+   console.log('Listening to port');
+  });
 });
